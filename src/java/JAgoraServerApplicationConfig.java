9ffd300f947c440e0b32e4606dcf5e801ff098b8
angle <angle@angle-Lenovo-IdeaPad-Y580>
2015-02-14 17:09:01 -0700

    Trying to debug connection failure.

---
+++ src/java/JAgoraServerApplicationConfig.java
@@ -0,0 +1,29 @@
+
+import java.util.HashSet;
+import java.util.Set;
+import javax.websocket.Endpoint;
+import javax.websocket.server.ServerApplicationConfig;
+import javax.websocket.server.ServerEndpointConfig;
+
+
+/**
+ *
+ * @author angle
+ */
+public class JAgoraServerApplicationConfig implements ServerApplicationConfig{
+
+    @Override
+    public Set<ServerEndpointConfig> getEndpointConfigs(Set<Class<? extends Endpoint>> endpointClasses) {
+        Set<ServerEndpointConfig> s = new HashSet<>();
+        //s.add(ServerEndpointConfig.Builder.create(JAgoraWebSocketServlet.class, "/Websocket/Agora").build());
+        return s;
+    }
+
+    @Override
+    public Set<Class<?>> getAnnotatedEndpointClasses(Set<Class<?>> scanned) {
+        Set<Class<?>> s = new HashSet<>();
+        s.add(JAgoraWebSocketServlet.class);
+        return s;
+    }
+    
+}
