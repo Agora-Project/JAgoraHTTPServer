01fdc3edfdaa60e4c85f70b2889d7d798e4e4721
Angular-Angel <ribbonwing@gmail.com>
2015-04-06 12:09:09 -0600

    Added License.

--- src/java/JAgoraHttpServer.java
+++ src/java/JAgoraHttpServer.java
@@ -1,3 +1,21 @@
+/*
+
+Copyright (C) 2015 Agora Communication Corporation
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
 
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -76,7 +94,7 @@ public class JAgoraHttpServer extends HttpServlet implements JAgoraServer {
     BasicBSONObject bsonRequest = JAgoraComms.readBSONObjectFromStream(inputStream);
     int requestType = (Integer) bsonRequest.get(IJAgoraLib.ACTION_FIELD);
     QueryResponder r = getResponder(requestType);
-    BasicBSONObject bsonResponse = null;
+    BasicBSONObject bsonResponse;
     if (r == null) {
       bsonResponse = new BasicBSONObject();
       bsonResponse.put(IJAgoraLib.RESPONSE_FIELD, IJAgoraLib.SERVER_FAIL);
